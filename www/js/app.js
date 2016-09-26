4951a38059ea31f20205e70c38690a9b89b3a6b1
Lalo177 <laloalanis22@gmail.com>
2016-09-26 12:47:43 -0500

    git
    git

---
+++ www/js/app.js
@@ -0,0 +1,11 @@
+
+
+
+
+var app = angular.module('bookStoreApp', []);
+
+	app.controller('StoreController', function($scope, $http) {
+		$http.get("js/libros.json").then(function (response) {
+			$scope.products = response.data.leerlibros;
+		});
+	});
